7e7baece84efab1bb6f7d822c916393c54bfb2c5
Deeksha Sridhar <dsri0006@student.monash.edu>
2021-09-20 19:59:46 +0800

    Editing the execute method of the DrinkEstusFlaskAction class

--- src/game/actions/DrinkEstusFlaskAction.java
+++ src/game/actions/DrinkEstusFlaskAction.java
@@ -4,26 +4,19 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 import game.items.EstusFlask;
+import game.actors.Player;
 
 public class DrinkEstusFlaskAction extends Action {
 
     private EstusFlask estusFlask;
-    private int maxhp;
 
-    /**
-     * Set value of maxhp
-     *
-     * @param maxhp- maximum hit points of player
-     */
-    public void setMaxHp(int maxhp) {
-        this.maxhp = maxhp;
-    }
 
     @Override
     public String execute(Actor actor, GameMap map) {
+        int maxHitPoints = Player.getHitPoints();
         int chargesLeft = estusFlask.getChargesLeft();
         if(chargesLeft >= 1){
-            actor.heal((40/100)*maxhp);
+            actor.heal((40/100)*maxHitPoints);
             chargesLeft =- 1;
         }
         else{
@@ -35,6 +28,15 @@ public class DrinkEstusFlaskAction extends Action {
     @Override
     public String menuDescription(Actor actor) {
         return actor + "drinks" + estusFlask + "(" + estusFlask.getChargesLeft() + "/" + estusFlask.getTotalCharges() + ")";
+    }
+
+    /**
+     * Returns the key used in the menu to trigger this Action.
+     *
+     * @return The key we use for this Action in the menu, or null to have it assigned for you.
+     */
+    public String hotkey() {
+        return "a";
     }
 }
 
