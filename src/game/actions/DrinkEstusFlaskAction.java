29488c713fcfa9245c42b1a56a6251e9f110a483
Deeksha Sridhar <dsri0006@student.monash.edu>
2021-09-20 16:57:52 +0800

    DrinkEstusFlaskAction class with execute and menuDescription methods

--- src/game/actions/DrinkEstusFlaskAction.java
+++ src/game/actions/DrinkEstusFlaskAction.java
@@ -3,15 +3,39 @@ package game.actions;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import game.items.EstusFlask;
 
 public class DrinkEstusFlaskAction extends Action {
+
+    private EstusFlask estusFlask;
+    private int maxhp;
+
+    /**
+     * Set value of maxhp
+     *
+     * @param maxhp- maximum hit points of player
+     */
+    public void setMaxHp(int maxhp) {
+        this.maxhp = maxhp;
+    }
+
     @Override
     public String execute(Actor actor, GameMap map) {
-        return null;
+        int chargesLeft = estusFlask.getChargesLeft();
+        if(chargesLeft >= 1){
+            actor.heal((40/100)*maxhp);
+            chargesLeft =- 1;
+        }
+        else{
+            System.out.println("Estus Flask doesn't have any charges left");
+        }
+        return menuDescription(actor);
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return null;
+        return actor + "drinks" + estusFlask + "(" + estusFlask.getChargesLeft() + "/" + estusFlask.getTotalCharges() + ")";
     }
 }
+
+
