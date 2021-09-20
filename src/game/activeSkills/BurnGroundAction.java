4a0331cf71f0f65a162320c0867e9ab5ccef3d8a
Deeksha Sridhar <dsri0006@student.monash.edu>
2021-09-20 16:57:09 +0800

    Constructor for BurnGroundAction class

--- src/game/activeSkills/BurnGroundAction.java
+++ src/game/activeSkills/BurnGroundAction.java
@@ -1,4 +1,27 @@
 package game.activeSkills;
 
-public class BurnGroundAction extends WeaponAction{
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.WeaponAction;
+import edu.monash.fit2099.engine.WeaponItem;
+
+public class BurnGroundAction extends WeaponAction {
+    /**
+     * Constructor
+     *
+     * @param weaponItem the weapon item that has capabilities
+     */
+    public BurnGroundAction(WeaponItem weaponItem) {
+        super(weaponItem);
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return null;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return null;
+    }
 }
