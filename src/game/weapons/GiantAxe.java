83fac4fe411a893e55a06d323e4cf74f842f0f7f
Deeksha Sridhar <dsri0006@student.monash.edu>
2021-09-21 21:44:52 +0800

    GiantAxe class with getAllowableActions and getActiveSkill methods

--- src/game/weapons/GiantAxe.java
+++ src/game/weapons/GiantAxe.java
@@ -1,8 +1,12 @@
 package game.weapons;
 
+import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.WeaponAction;
 import game.activeSkills.SpinAttackAction;
+import game.shopActions.PurchaseGiantAxeAction;
+
+import java.util.List;
 
 public class GiantAxe extends MeleeWeapon {
 
@@ -14,5 +18,21 @@ public class GiantAxe extends MeleeWeapon {
     @Override
     public WeaponAction getActiveSkill(Actor target, String direction) {
         return new SpinAttackAction(this);
+    }
+
+    // EDIT
+    @Override
+    public List<Action> getAllowableActions() {
+        boolean present = false;
+        for(Action action: allowableActions) {
+            if (action instanceof PurchaseGiantAxeAction) {
+                present = true;
+                break;
+            }
+        }
+        if(!present){
+            allowableActions.add(new PurchaseGiantAxeAction(new GiantAxe()));
+        }
+        return allowableActions.getUnmodifiableActionList();
     }
 }
